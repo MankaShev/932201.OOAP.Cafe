23d8257471de295da80bcb558f2cc61d5083f7f7
Anechka <officialposhta@mail.ru>
2024-11-02 13:41:36 +0700

    Добавлены комментарии

--- Cafe/Class1.cs
+++ Cafe/Class1.cs
@@ -5,43 +5,52 @@ using System.Linq;
 // Save this program as MainReturnValTest.cs.
 class MainReturnValTest
 {
-    static int Main()
+    static int Main()//то с чего начинается программа 
     {
-        Kitchen kitchen = new Kitchen();
-        Controller controller = new Controller(kitchen);
-
+        Kitchen kitchen = new Kitchen();//создание кухни с контроллером
+        Controller controller = kitchen.controller;//вытаскивание контролера с кухней внутри
+        //надо будет ещё в кухню добавить контроллер, т.к. она должна будет его вызывать, когда заказ будет готов
 
         Console.WriteLine("Welcome to our Onlne book&order service 'Goida'!!!\n");
         Console.WriteLine("Please sign in or create an account:\n");
 
+        //пользователь создаёт себе аккаунт или выбирает из БД в контроллере и помещает в переменную
         int ourVisitor;
 
         Console.WriteLine("Choose your table:\n");
 
+        //контролер связывается с БД и показывает свободные столики
         controller.data.show_tables();
 
-        //взять например с консоли
+        //пользователь выбирает столик и помещает в переменную
         int choosenTable;
 
+        //пока не прописала вывод меню (будет через интерфейс сделано) сделала так
         for (int i = 0; i < 5; i++)
         {
             Console.WriteLine("some kind of dish bla bla bla");
         }
 
-        //controller.show_menu();
-
-        int[] choosenDishes; //maybe buttons(?)
+        //controller.show_menu(); (пока не уверена как точно надо реализовать
 
 
-        //controller.getOrder(); // обращение к кухне установка таймера
+        //сюда помещаются выбранные блюда, что выберет пользователь
+        int[] choosenDishes;
 
+
+        //controller.Create_Order(); //создается заказ и идёт обращение к кухне, возврат времени готовки+вывод на экран
+
+        //С установкой таймера в коде МИ сказала не заморачиваться, поэтому делаем через вызов в main, когда заказ закончит создаваться
         //kitchen.settimer(); // conroller.dosmth();
+        //чуть позже распишу, надо просто чтобы вы тоже понимали, как у нас детально происходит обработка заказа
+        //1)контроллером 
+        //2)кухней - если непонятно по коду - пишите
 
 
 
-
+        //пример реализации интерфейса
         //controller { 
-        
+
         //    show(menu or order) // show(Ishow w){ w.print()}
         //}
 
@@ -58,16 +67,18 @@ class MainReturnValTest
     }
 }
 
-
+//Блюдо
 public class Dish
 {
     public int Dish_ID;
-    public string Name;
-    public double Price;
-    public string Menu_Section;
+    public string Name;//Название
+    public double Price;//Цена
+    public int Menu_Sec_ID; //ID секции меню, чтобы было удобно сортировать и выводить
+    public string Menu_Section;//секция меню
     public int Time_Of_Cook;
-    public bool Is_Available;
+    public bool Is_Available; //Для проверки на наличие, вроде там в какой-то из диаграмм нужно If использовать, это будет при формировании заказа
 
+    //конструктор
     public Dish(int dishID, string name, double price, string menuSection, int timeOfCook)
     {
         Dish_ID = dishID;
@@ -82,13 +93,15 @@ public class Dish
 
 }
 
+// Посетитель
 public class Visitor
 {
     public int Visitor_ID;
     public string FIO;
-    public bool An_Adult;
-    public string Number;
+    public bool An_Adult; //Совершеннолетний или нет
+    public string Number; //телефон
 
+    //конструктор
     public Visitor(int visitor_ID, string fio, bool anadult, string number)
     {
         Visitor_ID = visitor_ID;
@@ -101,13 +114,15 @@ public class Visitor
 
 }
 
+//Стол
 public class Table
 {
     public int Table_ID;
-    public bool Is_Booked;
-    private string Location;
-    private int Capacity;
+    public bool Is_Booked; //Занят или нет, тоже для проверок при брони стола
+    private string Location;//расположение
+    private int Capacity; //вместимость на ... человек
 
+    //Конструктор
     public Table(int table_ID, bool is_Booked, string location, int capacity)
     {
         Table_ID = table_ID;
@@ -115,6 +130,7 @@ public class Table
         Location = location;
         Capacity = capacity;
     }
+
 
     public int GetCapacity()
     {
@@ -124,6 +140,8 @@ public class Table
     {
         Is_Booked = true;
     }
+
+    // Показать свободен ли этот стол, если свободен выведется его номер и кол-во человек
     public void show_free_table()
     {
         if (!Is_Booked)
@@ -133,35 +151,40 @@ public class Table
     }
 }
 
+
+//Кухня
 public class Kitchen
 {
     public int Kitchen_ID;
     public int[] Orders_IDs;
-    public int Workload;
+    public int Workload; //загруженность (прибавлять к времени исполнения заказа)
+    public Controller controller; //чтобы вызывать, когда заказ готов
 
+    //КОнструктор
     public Kitchen()
     {
         Kitchen_ID = 0;
         Orders_IDs = new int[10];
         Workload = 0;
+        controller = new Controller(this); //контроллер для кухни, который будет к ней привязан
     }
-    public Kitchen(int ID, int[] ordersIDs, int workload)
-    {
-        Kitchen_ID = ID;
-        Orders_IDs = ordersIDs;
-        Workload = workload;
-    }
+
+
+    //добавить метод для установки контроллера в уже созданную кухню
+    //так же отдельно метод для установки загруженности
+    //метод для расчета времени заказа после получения списка блюд
 }
 
+//Заказ
 public class Order
 {
-    static int ID = 0;
-    public int Order_ID;
-    public int Table_ID;
-    public int Visitor_ID;
-    public int[] Dish_List;
-    public bool Is_Ready;
-    public double Total_Price = 0;
+    static int ID = 0; //статичный счётчик для заказов, чтобы у каждого нового заказа был уникальный порядковый номер
+    public int Order_ID; //номер заказа
+    public int Table_ID; //номер столика
+    public int Visitor_ID; //номер посетителя
+    public int[] Dish_List; //список НОМЕРОВ блюд, не самих блюд
+    public bool Is_Ready; //готовность
+    public double Total_Price = 0; //итоговая цена
     public Order()
     {
         Order_ID = ID;
@@ -175,7 +198,7 @@ public class Order
     public Order(int table_id, int[] dish_ids, int visitor_id)
     {
         Order_ID = ID;
-        ID++;
+        ID++; //при создании нового заказа через конструктор увеличиваем счётчик на +1
         Table_ID = table_id;
         Visitor_ID = visitor_id;
         Is_Ready = false;
@@ -183,25 +206,42 @@ public class Order
 
 }
 
+
+//Контроллер
 public class Controller
 {
-    public Kitchen k;
-    public DataBase data = new DataBase();
+    public Kitchen k; //кухня, к которой он привязан
+    public DataBase data = new DataBase(); //база данных для контроллера
+    //конструктор с кухней
     public Controller(Kitchen kit)
     {
         k = kit;
     }
 
+    //метод на проверку и бронирование стола тут должен быть (если стол занят, то вывести это пользователю)
+
+    //Формирование заказа
     public void Create_Order(int Table_Number, int[] dishes_number, int visitor_id)
     {
         Order order = new Order(Table_Number, dishes_number, visitor_id);
+        //потом вызов метода у кухни k{поле объекта} для расчёта времени готовки заказа
+        //метод для внесения в заказа итоговой стоимости и вывода её на экран
+        //мб потверждение от пользователя об оплате (просто написать "да" в консоль)
+        //добавление заказа в data.orders{поле объекта} (БД с заказами)
+        //мб что-то ещё, не знаю, 
 
-        
     }
 
-    public double Get_Price(Dish dish)
+
+    //для подсчёта итогового чека
+    public double Get_Chek(int[] dishes_number)
     {
-        return dish.Price;
+        double chek = 0;
+        for (int i = 0; i < dishes_number.Length; i++)
+        {
+            chek += data.dishes.ElementAt(i).Price;
+        }
+        return chek;
     }
 
 
@@ -209,15 +249,16 @@ public class Controller
 }
 
 
-
+//База данных (МИ сказала так можно делать, но если будут идеи - опять же пишите в чат)
 public class DataBase
 {
-    Visitor[] visitors = new Visitor[10];
-    Table[] tables = new Table[10];
-    Dish[] dishes = new Dish[10];
-    Order[] orders = new Order[10];
+    Visitor[] visitors = new Visitor[10]; //гости
+    public Table[] tables = new Table[10]; //столы
+    public Dish[] dishes = new Dish[10]; //блюда
+    Order[] orders = new Order[10]; //заказы
     int n = 10;
 
+    //конструктор для 10 элементов
     public DataBase()
     {
         #region DBO
@@ -266,6 +307,7 @@ public class DataBase
             //Console.WriteLine("Table " + i + " " + tables.ElementAt(i).Is_Booked);
         }
     }
+    //конструктор для n элементов
     public DataBase(int n)
     {
         this.n = n;
@@ -315,6 +357,7 @@ public class DataBase
         }
     }
 
+    //вывод свободных столиков из БД
     public void show_tables()
     {
         for (int i = 0; i < n; i++)
